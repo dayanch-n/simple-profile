b222a8f87445da5527d628ec21b62983bdc1daa4
iamdayanch <dayanch-iam@gmail.com>
2022-06-24 14:16:01 +0800

    feat(added ability to uploud photo and select dob)

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,13 +1,12 @@
-const Button = ({ children, width, height }) => {
+const Button = (props) => {
+  const { children } = props
   return (
-    <div className="flex space-x-2 justify-center">
       <button
-        type="button"
-        className={`w-${width} h-${height} px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out`}
+        className={`flex space-x-2 justify-center px-6 py-2.5 bg-blue-600 text-white font-medium w-full text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out`}
+        {...props}
       >
         {children}
       </button>
-    </div>
   );
 };
 
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-useless-escape */
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+
 import Button from "../../components/button";
 import Card from "../../components/card";
 import DatePicker from "../../components/date-picker";
 import Input from "../../components/input";
+import getBase64 from "../../helpers/base64";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -38,105 +40,137 @@ const Register = () => {
     setFormData((prevItems) => ({ ...prevItems, [name]: value }));
   };
 
+  const onUploadPhoto = async (event) => {
+    const file = event.target.files[0];
+    const baseURL = await getBase64(file);
+    if (baseURL) {
+      const url = baseURL.split(",")[1];
+      setFormData((prevItems) => ({ ...prevItems, photo: url }));
+    }
+  };
+
   return (
     <div className="flex flex-col h-full w-full items-center justify-center space-y-6">
-     <h3 class="text-2xl font-medium text-gray-900 dark:text-white">Sign in to our account</h3>
+      <h3 className="text-2xl font-medium text-gray-900 dark:text-white">
+        Sign in to our account
+      </h3>
       <Card width="100">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-
-          <div className="flex items-center justify-center">
-            <label className="text-blue border-blue hover:bg-blue flex h-28 w-28 cursor-pointer flex-col items-center justify-center rounded-full border bg-white uppercase tracking-wide shadow-lg hover:text-grey">
-              <span style={{fontSize: '0.60rem'}} className="mt-2">Upload Photo</span>
-              <input name="photo" type="file" class="hidden" />
-            </label>
+          <div className="flex w-full items-center justify-center">
+            <div className="flex items-center overflow-hidden justify-center border-blue hover:bg-blue h-28 w-28 cursor-pointer flex-col rounded-full border bg-white uppercase tracking-wide shadow-lg hover:text-grey">
+              {formData.photo ? (
+                <img
+                  className="object-cover"
+                  src={`data:image/png;base64, ${formData.photo}`}
+                  alt="user profile"
+                />
+              ) : (
+                <label className="text-blue upparcase">
+                  <span style={{ fontSize: "0.60rem" }} className="mt-2">
+                    Upload Photo
+                  </span>
+                  <input
+                    name="photo"
+                    type="file"
+                    className="hidden"
+                    onChange={onUploadPhoto}
+                  />
+                </label>
+              )}
+            </div>
           </div>
 
           <div className="flex flex-col space-y-3">
+            <div>
+              <label
+                htmlFor="name"
+                className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+              >
+                Your Name
+              </label>
+              <Input
+                {...register("name", {
+                  required: "Please insert your name",
+                })}
+                placeholder="Name"
+                type="text"
+                name="name"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+              >
+                Your Email
+              </label>
+              <Input
+                {...register("email", {
+                  required: "Please insert your email",
+                })}
+                placeholder="example@gmail.com"
+                type="email"
+                name="email"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+              >
+                New Password
+              </label>
+              <Input
+                {...register("password", {
+                  required: "Please insert new password",
+                })}
+                placeholder="example@gmail.com"
+                type="password"
+                name="password"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="flex flex-row items-center justify-between space-x-2">
               <div>
                 <label
-                  htmlFor="name"
-                  className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+                  htmlFor="gender"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
                 >
-                  Your Name
+                  Gender
                 </label>
-                <Input
-                  {...register("name", {
+                <select
+                  {...register("gender", {
+                    required: "Please choose gender",
+                  })}
+                  name="gender"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  onChange={handleInputChange}
+                >
+                  <option defaultValue>Choose</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
+              <div>
+                <label
+                  htmlFor="countries"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                >
+                  Date of birth
+                </label>
+                <DatePicker
+                  {...register("dob", {
                     required: "Please insert your name",
                   })}
-                  placeholder="Name"
-                  type="text"
-                  name="name"
                   onChange={handleInputChange}
                 />
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
-                >
-                  Your Email
-                </label>
-                <Input
-                  {...register("email", {
-                    required: "Please insert your email",
-                  })}
-                  placeholder="example@gmail.com"
-                  type="email"
-                  name="email"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
-                >
-                  New Password
-                </label>
-                <Input
-                  {...register("password", {
-                    required: "Please insert new password",
-                  })}
-                  placeholder="example@gmail.com"
-                  type="password"
-                  name="password"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="flex flex-row items-center justify-between space-x-2">
-                <div>
-                  <label
-                    htmlFor="gender"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                  >
-                    Gender
-                  </label>
-                  <select
-                    {...register("gender", {
-                      required: "Please choose gender",
-                    })}
-                    name="gender"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    onChange={handleInputChange}
-                  >
-                    <option defaultValue>Choose</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="Other">Other</option>
-                  </select>
-                </div>
-                <div>
-                  <label
-                    htmlFor="countries"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                  >
-                    Date of birth
-                  </label>
-                  <DatePicker onSelect={handleInputChange} />
-                </div>
               </div>
             </div>
-          <Button width={"full"}>Register</Button>
+          </div>
+          <Button disabled={Object.values(formData).every(Boolean) ? false : true} type="Submit">Register</Button>
         </form>
       </Card>
     </div>
@@ -144,3 +178,4 @@ const Register = () => {
 };
 
 export default Register;
+
